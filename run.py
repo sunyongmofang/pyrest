bfc4c6a3e81da9ea59841fdd9f8008ccda0d1088
sunyong <sunyong@sunyongdeMBP.lan>
2020-06-20 23:56:59 +0800

    requirements.txt 补全

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api
+from flask_restful import Resource
+from flask_sqlalchemy import SQLAlchemy
 
 
 class HelloWorld(Resource):
@@ -16,6 +18,8 @@ class PyRest(object):
 
     def runRest(self):
         app = Flask(__name__)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://master:123456@127.0.0.1:5432/test'
+        db = SQLAlchemy(app)
         api = Api(app)
         api.add_resource(HelloWorld, '/')
         app.run(debug=True, host='0.0.0.0', port=6000)
